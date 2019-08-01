12a1c5ad840a1434646f99ca577d99ecb7463ef0
Pradeesh <pradeesh@prasys.info>
2019-08-01 17:09:04 +1200

    Update scraper_amazon_threading_version.py
    fixed the comment

--- scraper_amazon_threading_version.py
+++ scraper_amazon_threading_version.py
@@ -41,6 +41,7 @@ def get_random_user_agent():
                        'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36']
     return random.choice(user_agent_list)
 
+## TO DO - CONVERT THE FUNCTION INTO A SMARTER WAY TO MAKE IT CLEANER
 def getParsedHTML(url,asin):
     amazon_url = url + asin
 
